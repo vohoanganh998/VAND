94715e8332642436b781a604abdd5ec2d20e72fd
Hoang Anh <vohoanganh998@gmail.com>
2022-12-11 16:54:18 +0700

    - deloy vue router

--- src/router.js
+++ src/router.js
@@ -5,6 +5,10 @@ const router = createRouter({
   history: routerHistory,
   routes: [
     {
+      path: "/:catchAll(.*)",
+      redirect: "/",
+    },
+    {
       path: "/",
       name: "Countries",
       component: () => import("./modules/country/views/Countries.vue"),
