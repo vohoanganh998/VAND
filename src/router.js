2166e80bc1d8fecb02b99f6f36d3fbbc8481710b
Hoang Anh <vohoanganh998@gmail.com>
2022-12-11 16:30:19 +0700

    - deloy vue router

--- src/router.js
+++ src/router.js
@@ -1,12 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 const routerHistory = createWebHistory('/vand-test/');
+console.log(routerHistory.base)
 const router = createRouter({
   history: routerHistory,
   routes: [
-    {
-      path: "/:catchAll(.*)",
-      redirect: "/",
-    },
     {
       path: "/",
       name: "Countries",
